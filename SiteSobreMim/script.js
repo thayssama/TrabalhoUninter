9cf875f583f3a2c9dffbe3be2c0e480a28b81f25
thayssama <thayssamodenamaciel@gmail.com>
2025-07-03 16:41:16 -0300

    Delete SiteSobreMim directory

--- SiteSobreMim/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const botao = document.getElementById("botao");
-    const mensagem = document.getElementById("mensagem");
-
-
-    botao.addEventListener("click", function () {
-        mensagem.style.display = "block"
-    });
-}); 
